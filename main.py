7e3ca4d1956186126d0536ffc555edae82aa3850
Zach Greenhalge <zgreenhalge@gmail.com>
2017-09-26 14:44:30 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+# import game
+
+if __name__ == '__main__':
+	pygame.init()
+	screen = pygame.display.set_mode([400, 300])
+	done = False
+
+	while not done:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				done = True
+
+		pygame.display.flip()
