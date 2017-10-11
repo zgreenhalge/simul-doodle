fa58005fdd4e87a07c2a1f7589b86f6e17f2ffd0
Adam Whitney <ag.whitney2@gmail.com>
2017-10-11 12:39:23 -0400

    Moved event handling to event_handlers.py

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
-import pygame, sys
-from Managers import GameManager
+import pygame
 from pygame.locals import *
+
+from Managers import GameManager
+from Managers.event_handlers import handle_keys
+
 # import game
 
 if __name__ == '__main__':
-	pygame.init()
-	pygame.display.set_caption("Ayy Lmao")
-	screen = pygame.display.set_mode([500, 500], RESIZABLE)
-	clock = pygame.time.Clock()
-	done = False
-	ticks = 0
+    pygame.init()
+    pygame.display.set_caption("Ayy Lmao")
+    screen = pygame.display.set_mode([500, 500], RESIZABLE)
+    clock = pygame.time.Clock()
+    done = False
+    ticks = 0
 
-	while ticks < 100 and not done:
-		for event in pygame.event.get():
-			if event.type == QUIT:
-				done = True
-			if event.type == VIDEORESIZE:
-				screen = pygame.display.set_mode((event.w, event.h), RESIZABLE)
-			# TODO on middle mouse down, capture position; on middle mouse up, drag difference between two positions
+    while ticks < 100 and not done:
+        for event in pygame.event.get():
+            quit = handle_keys(event).get('quit')
+            resize = handle_keys(event).get('resize')
+            mmb_down = handle_keys(event).get('mmb_down')
+            mmb_drag = handle_keys(event).get('mmb_drag')
 
-		pygame.display.flip()
-		ticks += 1
-		if ticks % 10 == 0:
-			GameManager.update(screen)
-			print(ticks)
-		clock.tick(60) # TODO make this scale w user selected speed somehow
+            if quit:
+                done = True
+            if resize:
+                screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+            if mmb_down:
+                # These can be used elsewhere as long as the button is held
+                x0 = event.pos[0]
+                y0 = event.pos[1]
+            if mmb_drag:
+                xf = event.pos[0]
+                yf = event.pos[1]
+                print("A rectangle with sides {} and {}".format(abs(xf-x0), abs(yf-y0)))
+
+        pygame.display.update()     # No args: same as flip
+        ticks += 1
+        if ticks % 10 == 0:
+            GameManager.update(screen)
+            print(ticks)
+        clock.tick(60)  # TODO make this scale w user selected speed somehow
